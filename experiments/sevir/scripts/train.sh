4d52d573c8367a90420f81ce24bba56e90067296
So Takao <sotakao@caltech.edu>
2026-01-05 14:58:40 -0800

    added training scripts

---
+++ experiments/sevir/scripts/train.sh
@@ -0,0 +1,3 @@
+python train.py --sevir-dir /resnick/groups/astuart/sotakao/score-based-ensemble-filter/FlowDAS/experiments/weather_forecasting/data/sevir_lr \
+                --epochs 500
+                
---
+++ experiments/sqg/scripts/train.sh
@@ -0,0 +1,4 @@
+python train.py --data_dir /resnick/scratch/sotakao/sqg_train_data \
+                --hrly_freq 12 \
+                --periodic
+                
